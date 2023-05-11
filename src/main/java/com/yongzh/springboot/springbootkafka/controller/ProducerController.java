a26bbf084c8a20df1370ff641084b76659816f8f
wuhuqifei <1214086027@qq.com>
2023-05-11 23:35:27 +0800

    kafka v1.0

---
+++ src/main/java/com/yongzh/springboot/springbootkafka/controller/ProducerController.java
@@ -0,0 +1,11 @@
+package com.yongzh.springboot.springbootkafka.controller;
+
+/**
+ * @author Administrator
+ * @version 1.0
+ * @program: springboot-kafka
+ * @description:
+ * @date 2023/5/11 23:34
+ */
+public class ProducerController {
+}
